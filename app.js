8c97d4b40c755f158670c9f6188c1e0a0a3a275b
Omurakunov <zizomur@yandex.com>
2021-01-22 16:07:19 +0600

    the ready calculator

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 Vue.createApp({
   data() {
     return {
+      value: "",
       
+     
+    }
+  },
+  methods: {
+    write(num){
+      this.value = ` ${this.value + num}`
+    },
+    delAll(){
+      this.value = ''
+    },
+    writeDote(dote){
+      if( this.value.indexOf(dote) === -1  ) {
+      this.value = ` ${this.value + dote}`
+      }
+      
+    },
+    calculate() {
+      this.value = eval(this.value) 
+    },
+    percentage(){
+      this.value = Number(this.value) 
+      this.value = this.value / 100
     }
   }
+  
 }).mount('#app')
